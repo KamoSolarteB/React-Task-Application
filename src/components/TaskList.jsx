733354c4eb3dfe5c3fd5bda0d83d8af739105e7d
camo <camilo.solarte@claro.com.co>
2023-02-28 22:58:59 -0500

    Se muestra los datos de las tareas

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data)
+  }, [])
+
+  if (tasks.length === 0) {
+    return <h1>No tasks yet</h1>
+  }
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi first task",
+    description: "Mi first task",
+  },
+  {
+    id: 1,
+    title: "Mi second task",
+    description: "Mi second task",
+  },
+  {
+    id: 2,
+    title: "Mi third task",
+    description: "Mi third task",
+  },
+];
