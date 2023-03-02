6922570340e6011746dd0846187e9b750d18f1e8
camo <camilo.solarte@claro.com.co>
2023-03-01 20:38:33 -0500

    Ultimo detalle en data

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -1,17 +1,7 @@
 export const tasks = [
   {
     id: 0,
-    title: "My first task",
-    description: "Mi first task",
-  },
-  {
-    id: 1,
-    title: "My second task",
-    description: "Mi second task",
-  },
-  {
-    id: 2,
-    title: "My third task",
-    description: "Mi third task",
+    title: "Default Task",
+    description: "Default Description",
   },
 ];
